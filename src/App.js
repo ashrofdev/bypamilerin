9b97fe39b3e5476bd7875d59ac85cfba57439c7e
First Last <salmanashraf853@gmail.com>
2020-07-24 13:44:59 +0100

    seting up pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.scss';
+import React, { Component } from 'react';
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      bypamilerinnnn
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home/>
+        <Services/>
+        <Contact/>
+        <span>Under development...</span>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className="services">
+            Services page
+        </div>
+    );
+};
+
+export default Services;
